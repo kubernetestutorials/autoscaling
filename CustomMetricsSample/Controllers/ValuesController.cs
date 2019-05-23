09606ba86bfba14d4736cb08b734a5b9d4118d2c
Uladzimir Sukhau <uladzimir.sukhau@ihsmarkit.com>
2019-05-23 11:48:32 +0300

    .net core app

---
+++ CustomMetricsSample/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMetricsSample.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Base()
+        {
+            return "Hello World!";
+        }
+
+        [Route("/get")]
+        [HttpGet]
+        public ActionResult<double> Get()
+        {
+            return Counters.Counters.RequestsCounter.Value;
+        }
+
+        [Route("/add")]
+        [HttpGet]
+        public ActionResult<double> Add()
+        {
+            Counters.Counters.RequestsCounter.Inc();
+            return Counters.Counters.RequestsCounter.Value;
+        }
+
+        [Route("/remove")]
+        [HttpGet]
+        public ActionResult<double> Remove()
+        {
+            Counters.Counters.RequestsCounter.Dec();
+            return Counters.Counters.RequestsCounter.Value;
+        }
+
+        [Route("/set/{value}")]
+        [HttpGet]
+        public ActionResult<double> Set(int value)
+        {
+            Counters.Counters.RequestsCounter.Set(value);
+            return Counters.Counters.RequestsCounter.Value;
+        }
+    }
+}
---
+++ CustomMetricsSample/Counters/Counters.cs
@@ -0,0 +1,9 @@
+﻿using Prometheus;
+
+namespace CustomMetricsSample.Counters
+{
+    public static class Counters
+    {
+        public static readonly Gauge RequestsCounter = Metrics.CreateGauge("my_app_num_requests", "Number of requests.");
+    }
+}
